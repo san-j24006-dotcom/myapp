22851ac4e3da095a1a2e004c7e89f4e97d4b1a25
san-j24006-dotcom <san-j24006@sangi.jp>
2026-07-23 10:33:56 +0900

    Update GAS_URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // GASのデプロイURLをここに貼り付けてください
 const CONFIG = {
-  GAS_URL: "YOUR_GAS_URL_HERE"
+  GAS_URL: "https://script.google.com/macros/s/AKfycbzfohDYwHj3j9u2CVy4KbOs5zFh1_rKNBN4QFhOsxlANI-8-jPfPydIqjqRXmu1pbr02Q/exec"
 };
